00a9400a593107b964f77777beb0437c4439207d
Mark Ricard <ricard@uta.edu>
2023-02-26 18:51:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-print("Hello Git Master")
+print("Someone Messed this up")
+print("The file will cause conflicts")
 print("Git is killing me!")
 print("Will I ever learn git?")
 print("bye bye")
-for i in range(20):
+for i in range(100):
     print('Trump will look great in an orange jumpsuit, be sure to shave his head')
 for i in range(3):
     print("Frank Burns eats earth worms!")
