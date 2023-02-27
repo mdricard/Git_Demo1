625649fed74c8dc7c55759dcc5f2a99d4a58aa34
Mark Ricard <ricard@uta.edu>
2023-02-26 18:52:34 -0600

    branch_3 range220

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Hello Git Master")
 print("Git is killing me!")
 print("Will I ever learn git?")
 print("bye bye")
-for i in range(20):
+for i in range(220):
     print('Trump will look great in an orange jumpsuit, be sure to shave his head')
 for i in range(3):
     print("Frank Burns eats earth worms!")
