0045cb231cb32534d81623034b920b950e3e3af6
Mark Ricard <ricard@uta.edu>
2023-02-26 18:58:43 -0600

    branch_4 if test

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-print("Someone Messed this up")
-print("The file will cause conflicts")
+print("I really really Messed this up")
+print("The file will cause a few conflicts")
 print("Git is killing me!")
 print("Will I ever learn git?")
 print("bye bye")
-for i in range(100):
+for i in range(1000):
     print('Trump will look great in an orange jumpsuit, be sure to shave his head')
 for i in range(3):
-    print("Frank Burns eats earth worms!")
+    print("Frank Burns eats worms!")
+i = 2
+if i > 1:
+    print(i)
